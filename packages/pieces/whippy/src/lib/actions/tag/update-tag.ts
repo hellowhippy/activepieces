38757529b1bdb12913679eee186a37086d6315ad
Irzam Dawood <irzamdawood64@gmail.com>
2024-02-21 22:12:37 +0500

    feat: implement missing fields in actions

--- packages/pieces/whippy/src/lib/actions/tag/update-tag.ts
+++ packages/pieces/whippy/src/lib/actions/tag/update-tag.ts
@@ -6,17 +6,17 @@ This action updates a tag in Whippy. Tag ID is required.
 API Documentation: https://docs.whippy.ai/reference/updatetag
 */
 
-import { createAction, Property } from '@activepieces/pieces-framework';
+import { createAction, Property } from "@activepieces/pieces-framework";
 import { Tag } from '../../api/api';
-import { appAuth } from '../../../index';
+import { appAuth } from "../../..";
+
 
 export const updateTag = createAction({
-  name: 'update_tag',
+  name: 'update_tag', 
   auth: appAuth,
   displayName: 'Update Tag',
   description: 'Update Tag',
   props: {
-    // Properties to ask from the user
     getTagId: Property.ShortText({
       displayName: 'Tag ID',
       required: true,
@@ -28,65 +28,58 @@ export const updateTag = createAction({
     getName: Property.LongText({
       displayName: 'Tag Name',
       required: false,
-    }),
+    }), 
     getState: Property.StaticDropdown({
-      displayName: 'Tag State',
-      required: false,
-      options: {
-        options: [
-          {
-            label: 'archived',
-            value: 'archived',
-          },
-          {
-            label: 'active',
-            value: 'active',
-          },
-        ],
-      },
+        displayName: 'Tag State',
+        required: false,
+        options: {
+            options: [
+                {
+                    label: 'archived',    
+                    value: 'archived'
+                },
+                {
+                    label: 'active',
+                    value: 'active'
+                }
+            ]
+        }
     }),
   },
   async run(context) {
     const apiKey = context.auth;
     const tagId = context.propsValue['getTagId'];
     const colorCode = context.propsValue['getColor'];
-    const name = context.propsValue['getName'];
+    let name = context.propsValue['getName'];
     const state = context.propsValue['getState'];
 
-    if (name !== undefined) {
-      name.replace(/\s+/g, ' ').trim();
+    if(name !== undefined)
+    {
+        name = name.replace(/\s+/g,' ').trim()
     }
-    
     function isValidHexColor(color: string) {
-      const hexColorRegex = /^#[0-9A-Fa-f]{6}$/;
-      return hexColorRegex.test(color);
+        const hexColorRegex = /^#[0-9A-Fa-f]{6}$/;
+        return hexColorRegex.test(color);
     }
-
-    if (colorCode !== undefined) {
-      if (isValidHexColor(colorCode)) {
-        console.log('Valid color code:', colorCode);
-      } else {
-        console.error('Invalid color code:', colorCode);
-      }
+    if (colorCode !== undefined)
+    {
+        if (isValidHexColor(colorCode)) {
+            console.log('Valid color code:', colorCode);
+        } else {
+            console.error('Invalid color code:', colorCode);
+        }
     }
 
     try {
-      const response = await Tag.updateTag(
-        apiKey,
-        tagId,
-        colorCode,
-        name,
-        state
-      );
+      const response = await Tag.updateTag(apiKey, tagId, colorCode, name, state);
       if (response.success) {
-        return response.data;
+        return response.data; 
       } else {
         console.error(response.message);
         return false;
       }
     } catch (error) {
-      console.error(error);
-      return false;
+      throw new Error(`Failed to update tag: ${error}`);
     }
-  },
+  }
 });
